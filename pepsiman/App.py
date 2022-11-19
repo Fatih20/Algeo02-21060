f0e127b813172a3dedfff3359726f1af30a71dde
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2022-11-19 18:47:45 +0700

    feat: camera bonus

--- pepsiman/App.py
+++ pepsiman/App.py
@@ -1,15 +1,14 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
-
-from processing import *
+from plyer import filechooser
+from PIL import Image
 import time
 
+from processing import *
 
 class NormalWindow(Screen):
     testMatrixEigen = None
@@ -20,16 +19,22 @@ class NormalWindow(Screen):
 
     # Show test image
 
-    def showTestImage(self, pathname):
+    def getTestImage(self):
         try:
-            self.ids.testImage.source = pathname[0]
+            path = filechooser.open_file(title="Pick a test image", filters=["*.jpg", "*.png"])
+            self.ids.testImage.source = path[0]
+            temp = self.ids.testImage.source.split('\\')
+            self.ids.fileLabel.text = temp[len(temp)-1]
         except:
             pass
 
     # Save training set folder name
-    def saveFolder(self, pathname):
+    def getFolderName(self):
         try:
-            self.ids.folderName.text = pathname[0]
+            path = filechooser.choose_dir(title="Choose the folder containing your training set")
+            self.ids.folderName.text = path[0]
+            temp = self.ids.folderName.text.split('\\')
+            self.ids.folderLabel.text = temp[len(temp)-1]
         except:
             pass
 
@@ -54,85 +59,104 @@ class NormalWindow(Screen):
         self.ids.processingTime.text = f"{to_show} secs"
 
     # Process Folder
-    def processTrainingSet(self):
+    def startTrainingSet(self):
         if (self.ids.folderName.text != ""):
-            jam1 = jam2 = menit1 = menit2 = detik1 = detik2 = 0
+            self.ids.processingTime.text = "Loading..."
+            Clock.schedule_once(self.processTrainingSet, 0)
+    
+    def processTrainingSet(self, interval):
+        jam1 = jam2 = menit1 = menit2 = detik1 = detik2 = 0
 
-            timeArr = self.getCurrentTime()
-            jam1 = timeArr[0]
-            menit1 = timeArr[1]
-            detik1 = timeArr[2]
+        timeArr = self.getCurrentTime()
+        jam1 = timeArr[0]
+        menit1 = timeArr[1]
+        detik1 = timeArr[2]
 
-            # Proses training set
-            sampleFolder = self.ids.folderName.text
-            path_list = path_generator(sampleFolder)
-            flat_matrix_list = image_f_matrix_generator(sampleFolder)
-            average_matrix = average_flatten_generator(flat_matrix_list)
-            training_matrix = training_matrix_generator(
-                flat_matrix_list, average_matrix)
-            training_matrix_t = np.transpose(training_matrix)
-            covariant_acc = np.matmul(training_matrix_t, training_matrix)
-            ev, e = eigen_generator(covariant_acc, training_matrix)
-            y = y_generator(covariant_acc, training_matrix)
+        # Proses training set
+        sampleFolder = self.ids.folderName.text
+        path_list = path_generator(sampleFolder)
+        flat_matrix_list = image_f_matrix_generator(sampleFolder)
+        average_matrix = average_flatten_generator(flat_matrix_list)
+        training_matrix = training_matrix_generator(
+            flat_matrix_list, average_matrix)
+        training_matrix_t = np.transpose(training_matrix)
+        covariant_acc = np.matmul(training_matrix_t, training_matrix)
+        ev, e = eigen_generator(covariant_acc, training_matrix)
+        y = y_generator(covariant_acc, training_matrix)
 
-            self.testMatrixEigen = e
-            self.testMatrixY = y
-            self.testPathList = path_list
-            self.testMatrixAverage = average_matrix
-            self.testInserted = True
+        self.testMatrixEigen = e
+        self.testMatrixY = y
+        self.testPathList = path_list
+        self.testMatrixAverage = average_matrix
+        self.testInserted = True
 
-            timeArr = self.getCurrentTime()
-            jam2 = timeArr[0]
-            menit2 = timeArr[1]
-            detik2 = timeArr[2]
+        timeArr = self.getCurrentTime()
+        jam2 = timeArr[0]
+        menit2 = timeArr[1]
+        detik2 = timeArr[2]
 
-            self.subtractAndShowTime(
-                jam2, jam1, menit2, menit1, detik2, detik1)
+        self.subtractAndShowTime(
+            jam2, jam1, menit2, menit1, detik2, detik1)
+        
+    # Search for best 
+    def startBestImage(self):
+        if (self.ids.testImage.source != "Images/placeHolderImage.png" and self.testInserted == True):
+            self.ids.processingTime.text = "Loading..."
+            Clock.schedule_once(self.searchBestImage, 0)
 
-    # Search for best image
-    def searchBestImage(self):
-        if (self.ids.testImage.source != "Images/LoadingIcon.png" and self.testInserted == True):
-            # Clock.schedule_interval(self.showLoading(), 0)
-            jam1 = jam2 = menit1 = menit2 = detik1 = detik2 = 0
+    def searchBestImage(self, interval):
+        jam1 = jam2 = menit1 = menit2 = detik1 = detik2 = 0
 
-            timeArr = self.getCurrentTime()
-            jam1 = timeArr[0]
-            menit1 = timeArr[1]
-            detik1 = timeArr[2]
+        timeArr = self.getCurrentTime()
+        jam1 = timeArr[0]
+        menit1 = timeArr[1]
+        detik1 = timeArr[2]
 
-            # time.sleep(3)
+        # time.sleep(3)
 
-            # Cari bestface ada di sini
-            testedImage = self.ids.testImage.source
-            file_of_bestface = bestface(
-                self.testMatrixAverage, self.testMatrixEigen, self.testMatrixY, self.testPathList, testedImage)
-            self.ids.resultImage.source = file_of_bestface
+        # Cari bestface ada di sini
+        testedImage = self.ids.testImage.source
+        file_of_bestface = bestface(
+            self.testMatrixAverage, self.testMatrixEigen, self.testMatrixY, self.testPathList, testedImage)
+        self.ids.resultImage.source = file_of_bestface
 
-            timeArr = self.getCurrentTime()
-            jam2 = timeArr[0]
-            menit2 = timeArr[1]
-            detik2 = timeArr[2]
+        timeArr = self.getCurrentTime()
+        jam2 = timeArr[0]
+        menit2 = timeArr[1]
+        detik2 = timeArr[2]
 
-            self.subtractAndShowTime(
-                jam2, jam1, menit2, menit1, detik2, detik1)
+        self.subtractAndShowTime(
+            jam2, jam1, menit2, menit1, detik2, detik1)
 
 
 class CameraWindow(Screen):
-    # testMatrixEigen = None
-    # testMatrixY = None
-    # testPathList = []
-    # testMatrixAverage = None
-    # testInserted = False
+    testMatrixEigen = None
+    testMatrixY = None
+    testPathList = []
+    testMatrixAverage = None
+    testInserted = False
 
-    # def startCapturing(self):
-    #     if (self.cameraMatrixEigen != None and self.ids.camera.play == True):
-    #         Clock.schedule_interval(self.processCaptureImage, 10)
+    def startCapturing(self):
+        if (self.testInserted == True): #and self.ids.camera.play == True):
+            Clock.schedule_interval(self.processCaptureImage, 2)
 
-    # def processCaptureImage(self, interval):
-    #     print('a')
+    def processCaptureImage(self, interval):
+        self.ids.camera.export_to_png("Images/cameraImage.png")
+        png = Image.open("Images/cameraImage.png").crop((256, 176, 512, 432)).convert("RGBA")
+        bg = Image.new('RGBA', png.size, (255, 255, 255))
+        alpha = Image.alpha_composite(bg, png).convert("RGB")
+        alpha.resize((256, 256)).save("Images/cameraImage.jpg", "JPEG", quality = 100)
 
-    # def stopCapturing(self):
-    #     Clock.unschedule(self.processCaptureImage)
+        testedImage = "Images/cameraImage.jpg"
+
+        file_of_bestface = bestface(
+            self.testMatrixAverage, self.testMatrixEigen, self.testMatrixY, self.testPathList, testedImage)
+        self.ids.cameraResultImage.source = file_of_bestface
+
+        pass
+
+    def stopCapturing(self):
+        Clock.unschedule(self.processCaptureImage)
     
     pass
 
